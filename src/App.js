4b217b4c3a58b278fba5b24b6d47b33ca0f6aaaf
valnicky <valery0007@gmail.com>
2022-01-22 18:03:47 +0100

    add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import Header from './components/Header';
+import React from 'react';
+import Header from './components/Header.jsx';
+import './App.css';
 
 function App() {
     return ( <div className = "app" >
-              <Header/>
+              <Header />
         </div>
       );
 }
