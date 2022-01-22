407037dff6f1985c184100c189dd51f46fc9a174
valnicky <valery0007@gmail.com>
2022-01-22 16:16:02 +0100

    add styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from './components/Header';
 
 function App() {
-    return ( <div className = "App" >
+    return ( <div className = "app" >
               <Header/>
         </div>
       );
